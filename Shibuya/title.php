f0b7df558ffc786146962a35fc5f2a0f139d3573
DESKTOP-PC7ETBQ\sdaik <s.daiki.0816@gmail.com>
2019-01-28 11:08:54 +0900

    タイトルだけStyleSheet埋め込み

--- Shibuya/title.php
+++ Shibuya/title.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+<link rel="stylesheet" type="text/css" href="Style.css">
 <head>
 	<title>Login</title>
 </head>
@@ -7,8 +8,8 @@
 
 <h1>テスト</h1>
 <form action="hitblow.php" method="POST">
-	USERNAME   :<input type="text"  name="username" value="<?= $_COOKIE['username'] ?>"><br>
-	NUMBER(3桁):<input type="text"  name="number"><br>
+	<p>USERNAME   :<input type="text"  name="username" value="<?= $_COOKIE['username'] ?>"><br></p>
+	<p>NUMBER(3桁):<input type="text"  name="number"><br></p>
 	<button>ログイン</button>
 </form>
 
